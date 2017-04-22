7bf57b7a7c5c6f7b92b740fbff432e43b6960893
jianfeng_huang <jianfeng_huang@kingdee.com>
2017-04-22 13:13:05 +0800

    create canvas

---
+++ src/views/Canvas.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="canvas-box">
+        <h1>canvas：</h1>
+        <canvas id="canvas-panel" width="400px" height="400px" style="border: 1px solid #ccc;"></canvas>
+    </div>
+</template>
+<script>
+    export default{
+        data () {
+            return {};
+        },
+        created () {
+
+        },
+        methods: {
+
+        },
+        components: {}
+    };
+</script>
+<style>
+
+</style>
